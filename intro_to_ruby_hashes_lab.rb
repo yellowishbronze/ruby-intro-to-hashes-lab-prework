3205e26b992e23a0af3aa50c97d85f10f952c3d5
Bryan Teng <bteng1226@gmail.com>
2018-06-25 00:43:08 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ end
 
 def monopoly
 	monopoly = {
-	  :railroads = {
+	  :railroads => {
 	  }
 	}
 
@@ -16,7 +16,7 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	  :railroads ={
+	  :railroads => {
 	    pieces: "4"
 	    names: {}
 	    rent_in_dollars: {}
